722bfd9eed1bc2d742385c5a7ee6851667ce0114
MaxiHbra <maxihuebra2003@gmail.com>
2026-08-20 19:35:06 -0300

    Terminando capitulo uno

---
+++ Pŕactica/numeros/prueba_long.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+	using namespace std;
+	int main(){
+		int a = 123456789; //Da un error en la salida ya que estamos operando en b un int
+		long long b = a*a;
+		cout << b << "\n";
+		return 0; 
+	}
